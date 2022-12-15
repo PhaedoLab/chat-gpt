728be8d7aa71609888ffb5b03d856d735744e9b7
web3dever <lezliemoudryfpv88@proton.me>
2022-12-15 17:22:35 +0800

    feat: modify

--- src/assets/utils/openai.js
+++ src/assets/utils/openai.js
@@ -1,11 +1,11 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 
-let a = 'sk'
-let b = '-mu95BgsT5'
-let c = 'zFyoycYsg9HT3BlbkFJt8odzWYNabXLeBLnTE9Z'
+// let a = 'sk'
+// let b = '-mu95BgsT5'
+// let c = 'zFyoycYsg9HT3BlbkFJt8odzWYNabXLeBLnTE9Z'
 const configuration = new Configuration({
-  apiKey: atob('c2stbXU5NUJnc1Q1ekZ5b3ljWXNnOUhUM0JsYmtGSnQ4b2R6V1lOYWJYTGVCTG5URTla')
+  // apiKey: atob('c2stbXU5NUJnc1Q1ekZ5b3ljWXNnOUhUM0JsYmtGSnQ4b2R6V1lOYWJYTGVCTG5URTla')
   
 });
 const openai = new OpenAIApi(configuration);
